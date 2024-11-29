a6aa41db627df048133f174183407342090becbf
sakhi baksh <sakhibux09@gmail.com>
2024-11-29 10:28:35 -0800

    my third commit
    29-11-2024

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useState } from "react";
+const Clock = () => {
+    const curTime = new Date().toLocaleTimeString();
+    const [time, setTime] = useState(curTime);
 
-export default App;
+    const update = () =>{
+        const curTime = new Date().toLocaleTimeString();
+        setTime(curTime)
+    }
+    setInterval(() => {
+      update()  
+    }, 1000); 
+    return (
+        <>
+        <div className="container">
+            <h1><b>SAKHI</b></h1>
+            <h1 className="text">Digital Clock</h1>
+            <h1>{time}</h1>
+        </div>
+        </>
+    )
+}
+export default Clock
